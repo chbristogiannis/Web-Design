3846ba13973c6f056907b8f56d4d59b64053da7e
Giorgos-Rousias <rousias1994@gmail.com>
2024-09-05 23:25:54 +0300

    makes navbar a component

---
+++ src/components/NavBar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="top-navbar">
+            <ul>
+                <li>
+                <NavLink 
+                    to="/UserHomePage" 
+                    className={({ isActive }) => isActive ? "active-link" : ""}
+                >
+                    Αρχική Σελίδα
+                </NavLink>
+                </li>
+                <li>
+                <NavLink 
+                    to="/NetworkPage" 
+                    className={({ isActive }) => isActive ? "active-link" : ""}
+                >
+                    Δίκτυο
+                </NavLink>
+                </li>
+                <li>
+                <NavLink 
+                    to="/JobListingsPage" 
+                    className={({ isActive }) => isActive ? "active-link" : ""}
+                >
+                    Αγγελίες
+                </NavLink>
+                </li>
+                <li>
+                <NavLink 
+                    to="/ConversationsPage" 
+                    className={({ isActive }) => isActive ? "active-link" : ""}
+                >
+                    Συζητήσεις
+                </NavLink>
+                </li>
+                <li>
+                <NavLink 
+                    to="/NotificationsPage" 
+                    className={({ isActive }) => isActive ? "active-link" : ""}
+                >
+                    Ειδοποιήσεις
+                </NavLink>
+                </li>
+                <li>
+                <NavLink 
+                    to="/PersonalDetailsPage" 
+                    className={({ isActive }) => isActive ? "active-link" : ""}
+                >
+                    Προσωπικά Στοιχεία
+                </NavLink>
+                </li>
+                <li>
+                <NavLink 
+                    to="/SettingsPage" 
+                    className={({ isActive }) => isActive ? "active-link" : ""}
+                >
+                    Ρυθμίσεις
+                </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/pages/UserHomePage/UserHomePage.js
+++ src/pages/UserHomePage/UserHomePage.js
@@ -1,93 +1,79 @@
 // src/UserHomePage.js
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext';  // Adjust the import path
+import Navbar from '../../components/NavBar';  // Adjust the import path
+import { useNavigate } from 'react-router-dom';
 import './UserHomePage.css';
 
 function HomePage() {
-  const {user, isAuthenticated, loading} = useAuth();
+	const {user, isAuthenticated, loading} = useAuth();
+	const navigate = useNavigate();
 
-  // const [userData, setUserData] = useState({
-  //   fullName: user.firstName + ' ' + user.lastName,
-  //   photo: user.photo || 'https://via.placeholder.com/100',
-  // });
+	useEffect(() => {
+			if (loading) {
+				// Handle loading state (e.g., show a loader)
+			}
+		}, [loading]);
 
-  // useEffect(() => {
-  //   const fetchUserData = async () => {
+	return (
+		<div className="homepage-container">
+			<Navbar />
+			<div className="content-container">
+				<aside className="left-sidebar">
+					<div className="user-info">
+						<div className="user-profile">
+							<img
+								src={user.photo}
+								alt="Profile"
+								className="profile-picture"
+							/>
+							<h3>{user.firstName}</h3>
+						</div>
+						<h3>Προσωπικά Στοιχεία</h3>
+						<div className="redirect-button-container">
+							<button onClick={() => navigate('/PersonalDetailsPage')}>
+								Δείτε το προφίλ σας
+							</button>
+						</div>
+					</div>
+					<div className="user-network">
+						<h3>Το Δίκτυό σας</h3> 
+						<div className="redirect-button-container">
+							<button onClick={() => navigate('/NetworkPage')}>
+								Δείτε το δίκτυό σας
+							</button>
+						</div>
+					</div>
+				</aside>
 
-  //     setUserData(userData);
-  //   };
+				<main className="main-content">
+					<div className="timeline">
+						<h3>Το Χρονολόγιο σας</h3>
+						<div className="post-creation">
+							<textarea placeholder="Δημιουργήστε ένα νέο κείμενο ..."></textarea>
+							<div className="media-upload">
+								<button>Φωτογραφία</button>
+								<button>Βίντεο</button>
+								<button>Αρχείο Ήχου</button>
+							</div>
+							<button className="submit-post">Αναρτήστε</button>
+						</div>
 
-  //   fetchUserData();
-  // }, []);
-
-  useEffect(() => {
-        if (loading) {
-            // Handle loading state (e.g., show a loader)
-        }
-    }, [loading]);
-
-  return (
-    <div className="homepage-container">
-      <nav className="top-navbar">
-        <ul>
-          <li><a href="/UserHomePage">Αρχική Σελίδα</a></li>
-          <li><a href="/NetworkPage">Δίκτυο</a></li>
-          <li><a href="/JobListingsPage">Αγγελίες</a></li>
-          <li><a href="/ConversationsPage">Συζητήσεις</a></li>
-          <li><a href="/NotificationsPage">Ειδοποιήσεις</a></li>
-          <li><a href="/PersonalDetailsPage">Προσωπικά Στοιχεία</a></li>
-          <li><a href="/SettingsPage">Ρυθμίσεις</a></li>
-        </ul>
-      </nav>
-      
-      <div className="content-container">
-        <aside className="left-sidebar">
-          <div className="user-info">
-            <div className="user-profile">
-              <img
-                src={user.photo}
-                alt="Profile"
-                className="profile-picture"
-              />
-              <h3>{user.firstName}</h3>
-            </div>
-            <h3>Προσωπικά Στοιχεία</h3>
-            <p><a href="/PersonalDetailsPage">Δείτε το προφίλ σας</a></p>
-          </div>
-          <div className="user-network">
-            <h3>Το Δίκτυό σας</h3>
-            <p><a href="/NetworkPage">Δείτε το δίκτυό σας</a></p>
-          </div>
-        </aside>
-
-        <main className="main-content">
-          <div className="timeline">
-            <h3>Το Χρονολόγιο σας</h3>
-            <div className="post-creation">
-              <textarea placeholder="Δημιουργήστε ένα νέο κείμενο ..."></textarea>
-              <div className="media-upload">
-                <button>Φωτογραφία</button>
-                <button>Βίντεο</button>
-                <button>Αρχείο Ήχου</button>
-              </div>
-              <button className="submit-post">Αναρτήστε</button>
-            </div>
-
-            <div className="posts">
-              <div className="post">
-                <h4>Άρθρο από επαγγελματία</h4>
-                <p>Κείμενο του άρθρου...</p>
-                <div className="post-actions">
-                  <button>Σχόλιο</button>
-                  <button>Ενδιαφέρον</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-      </div>
-    </div>
-  );
+						<div className="posts">
+							<div className="post">
+								<h4>Άρθρο από επαγγελματία</h4>
+								<p>Κείμενο του άρθρου...</p>
+								<div className="post-actions">
+								<button>Σχόλιο</button>
+								<button>Ενδιαφέρον</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</main>
+			</div>
+		</div>
+	);
 }
 
 export default HomePage;
